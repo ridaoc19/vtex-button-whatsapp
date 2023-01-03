e458c2ab382b2a83e4903f622018722ad5e2de0b
ridaoc19 <ricardo.david@itglobers.com>
2023-01-03 03:56:55 -0500

    feat: creation button whatsapp

---
+++ react/WhatsappButton.tsx
@@ -0,0 +1,4 @@
+/* eslint-disable prettier/prettier */
+import WhatsappButton from './components/WhatsappButton'
+
+export default WhatsappButton
---
+++ react/components/WhatsappButton.tsx
@@ -0,0 +1,76 @@
+/* eslint-disable prettier/prettier */
+import React from 'react'
+import PropTypes from 'prop-types'
+
+// Define the data types props the component
+type Props = {
+  logo: string
+  phone: string
+  message: string
+  width: number
+  height: number
+}
+
+// Define the functional component
+const WhatsappButton = ({ logo, phone, message, width, height }: Props) => {
+  const formattedMessage = message.replace(/ /g, '%20')
+
+  return (
+    <>
+      <div className="fixed bottom-1 left-1">
+        <a
+          href={`https://wa.me/${phone}?text=${formattedMessage}`}
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img src={logo} width={width} height={height} alt="Whatsapp Logo" />
+        </a>
+      </div>
+    </>
+  )
+}
+
+// Define the data types validations
+WhatsappButton.propTypes = {
+  logo: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired,
+  message: PropTypes.string,
+  width: PropTypes.number,
+  height: PropTypes.number,
+}
+
+// Define the default props values
+WhatsappButton.defaultProps = {
+  logo: 'undefined',
+  phone: '3022964317',
+  message: 'Hola, bienvenido a Tiendas D1. En qué podemos ayudarle',
+  width: 80,
+  height: 80,
+}
+
+// Define schema for backoffice admin
+WhatsappButton.schema = {
+  title: 'Whatsapp Button',
+  type: 'object',
+  properties: {
+    logo: {
+      title: 'Whatsapp logo',
+      type: 'string',
+      widget: {
+        'ui:widget': 'image-uploader',
+      },
+    },
+    phone: {
+      title: 'Sales support whatsapp phone',
+      type: 'string',
+    },
+    message: {
+      title: 'Message sent to client',
+      type: 'string',
+      widget: {
+        'ui:widget': 'textarea',
+      },
+    },
+  },
+}
+export default WhatsappButton
